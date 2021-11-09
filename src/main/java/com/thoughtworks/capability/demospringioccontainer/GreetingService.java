e199bd8e15861d10cb1448087241c5ed3279fe1e
Yongqiang Zhu <yongqiang.zhu@thoughtworks.com>
2021-11-09 10:21:43 +0800

    [yongqiang] feat: use proxy to inject prototype bean into singleton

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
 
     String sayHi() {
-        return "hello world";
+        return "hello world" + this.hashCode();
     }
 }
