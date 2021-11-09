ef450bbce3bfc68ebfd72d45f6001bf556d43c2f
Yongqiang Zhu <yongqiang.zhu@thoughtworks.com>
2021-11-09 10:15:35 +0800

    [yongqiang] feat: use Lookup to inject prototype bean into singleton

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
-        return "hello world";
+        return "hello world" + this.hashCode();
     }
 }
